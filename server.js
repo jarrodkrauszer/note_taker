dc2c47f2f3967b03e0697dffb2a52132c85b1e5a
jarrodkrauszer <96881754+jarrodkrauszer@users.noreply.github.com>
2023-10-06 10:00:15 -0400

    worked on adding the delete feature

--- server.js
+++ server.js
@@ -14,9 +14,6 @@ app.use(express.json());
 //   res.sendFile(path.join(__dirname, './public/index.html'));
 // });
 
-
-
-
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, './public/notes.html'));
 });
